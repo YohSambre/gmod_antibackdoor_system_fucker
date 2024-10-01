6a1693cae74a491c219a22eef87d0639ccb9df20
YohSambre <YohSambre@users.noreply.github.com>
2024-10-01 11:32:07 +0200

    Add files via upload

---
+++ ez_antibackdoor/antibackdoorkill/lua/autorun/client/ez.lua
@@ -0,0 +1,11 @@
+--I created this code about 2/3 years ago, it is so simple and yet it works very well
+function FuckOff(ply)
+  Msg( "\n" )
+end
+concommand.Add( "cpe_menu", FuckOff ) -- https://steamcommunity.com/sharedfiles/filedetails/?id=1714628992
+concommand.Add( "nomalua_scan", FuckOff ) -- https://github.com/THABBuzzkill/nomalua
+concommand.Add( "braxscan", FuckOff ) -- https://github.com/YohSambre/braxscan
+concommand.Add( "braxscan all 1", FuckOff ) -- ^
+concommand.Add( "gac_checkbackdoors", FuckOff ) -- https://github.com/GlorifiedPig/GlorifiedAnticheat
+concommand.Add( "bs_scan_full", FuckOff ) -- https://github.com/Xalalau/backdoor-shield
+concommand.Add( "bs_scan", FuckOff ) -- ^
